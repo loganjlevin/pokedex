e00ea888f20b196634c70e8d40e22974f89230bd
Logan Levin <loganjlevin@gmail.com>
2023-02-10 14:51:57 -0800

    created pokemonList array and populated it with some pokemon objects

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # pokedex
+
+Project description
+
+How to get the project running
+
+Project dependencies
+
+Which API the project uses
--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,27 @@
-alert("Hello world!");
-let favoriteFood = "burrito";
-document.write(favoriteFood);
+let pokemonList = [];
+
+let pokemon = {
+  name: 'Bulbasaur',
+  height: 0.7,
+  weight: 6.9,
+  type: ['grass', 'poison'],
+};
+pokemonList[0] = pokemon;
+
+pokemon = {
+  name: 'Charmander',
+  height: 0.6,
+  weight: 8.5,
+  type: ['fire'],
+};
+pokemonList[1] = pokemon;
+
+pokemon = {
+  name: 'Squirtle',
+  height: 0.5,
+  weight: 9,
+  type: ['water'],
+};
+pokemonList[2] = pokemon;
+
+console.log(pokemonList);
